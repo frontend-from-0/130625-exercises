8f4fea59ce93527673bbb06ddb2fb16b43653262
Your Name <your.email@example.com>
2025-11-18 17:26:19 +0300

    Adding personal info datas and disables/enabled button according to given infos

--- Lesson21/nurbasak/script.js
+++ Lesson21/nurbasak/script.js
@@ -6,6 +6,10 @@
 3. Take data from current element and show it in the target output field.
 
 */
+
+const onlyLetters = /^[a-zA-Z\s]+$/;
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 const dateInput = document.getElementById('date');
 const dateOutput = document.getElementById('selected-date');
 const timeslotButtons = document.querySelectorAll('.slot');
@@ -16,13 +20,69 @@ const confirmedDate = document.getElementById('confirmed-date');
 const confirmedTime = document.getElementById('confirmed-time');
 const confirmButton = document.getElementById('confirm');
 
+const nameInput = document.getElementById("name");
+const nameInputError = document.getElementById("nameError");
+const nameOutput = document.getElementById("given-username");
+const emailInput = document.getElementById("email");
+const emailInputError = document.getElementById("emailError");
+const emailOutput = document.getElementById("given-email");
+const confirmedName = document.getElementById('confirmed-name');
+const confirmedEmail = document.getElementById('confirmed-email');
+
+let formValid = true;
 
 // TODO: set min date for tomorrow or any other future date
+
 
 const data = {
   date: null,
   time: null,
+  name: null,
+  email:null,
 };
+
+//added personal info part to the form
+
+function validateName(name,nameError) {
+  if (!onlyLetters.test(name)) {
+    nameError.innerText = 'Please enter a valid username.';
+    nameError.classList.remove('hidden');
+    formValid = false;
+  } else if (!(name.trim().length > 0)) {
+    nameError.classList.remove('hidden');
+    nameError.innerText = 'Enter more than one character.';
+    formValid = false;
+  } else {
+    nameError.innerText = '';
+    nameError.classList.add('hidden');
+  }
+
+  nameInput.addEventListener('change', () => {
+    nameOutput.textContent = nameInput.value;
+    data.name = nameInput.value;
+    allowSubmit();
+  })
+};
+
+nameInput.addEventListener('input', () => validateName(nameInput.value, nameInputError));
+
+function validateEmail(email,emailError) {
+  if (!emailPattern.test(email)) {
+    emailError.innerText = 'Please enter a valid email address.';
+    emailError.classList.remove('hidden');
+    formValid = false;
+  } else {
+    emailError.innerText = '';
+    emailError.classList.add('hidden');
+  }
+  emailInput.addEventListener('change', () => {
+    emailOutput.textContent=emailInput.value;
+    data.email = emailInput.value;
+    allowSubmit();
+  })
+};
+
+emailInput.addEventListener('input', () => validateEmail(emailInput.value, emailInputError));
 
 //added functionality to show date line on click if supported
 
@@ -61,17 +121,20 @@ function showSelectedTime(element) {
 }
 
 function allowSubmit() {
-  if (data.date && data.time) {
+  if (data.date && data.time && data.name && data.email) {
     confirmButton.removeAttribute('disabled');
   }
 }
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  if (!data.date && !data.time) return;
+  if (!data.date && !data.time && !data.name && !data.email) return;
 
   form.classList.add('hidden');
   confirmation.classList.remove('hidden');
+  confirmedName.textContent=data.name.textContent;
+  confirmedEmail.textContent=data.email;
   confirmedDate.textContent = data.date;
   confirmedTime.textContent = data.time;
 });
+
